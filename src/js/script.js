efbc14cfb95f86c43f7115a93ad98a4fad887c83
Victor <barosv@yandex.ru>
2022-05-20 19:41:29 +0300

    готов адаптив без модальных окон

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Counter
 
-    const btnMinus = document.querySelector('[data-action="minus"]');
-    const btnPlus = document.querySelector('[data-action="plus"]');
-    const counter = document.querySelector('[data-counter]');
+    const btnMinus = [...document.querySelectorAll('[data-action="minus"]')];
+    const btnPlus = document.querySelectorAll('[data-action="plus"]');
+    const counter = document.querySelectorAll('[data-counter]');
 
-    btnMinus.addEventListener('click', () => {
-        if (parseInt(counter.innerText) > 1) {
-            counter.innerText = --counter.innerText;
-        }
+    btnMinus.forEach(item => {
+        item.addEventListener('click', () => {
+            if (parseInt(counter[i].innerText) > 1) {
+                counter[i].innerText = --counter[i].innerText;
+            }
+        });
     });
 
-    btnPlus.addEventListener('click', () => {
-        counter.innerText = ++counter.innerText;
+    btnPlus.forEach((item, i) => {
+        item.addEventListener('click', () => {
+            counter[i].innerText = ++counter[i].innerText;
+        });
     });
 
     // Modal
